3813b009b131a48bf52e96ca77d4325d5dbebf96
blkis169 <qees169@gmail.com>
2025-03-23 03:41:49 +0300

    i added more in my promise

--- index2.js
+++ index2.js
@@ -1,15 +1,19 @@
-let button = document.querySelector("button");
+let button = document.querySelector(".first-button");
 let first_div = document.querySelector(".par");
 let second_div = document.querySelector(".par_the_s")
 let arrow = document.querySelector(".arrow");
 let td = document.querySelectorAll("td")
 let xoArray = ["X", "O", "X", "O", "X", "O", "X", "O", "X"]
-
+let endgame = document.querySelector(".the-end");
+let finalbut = document.querySelector(".final-button")
 
 
 
 
 //التحويل بين الصفحتين 
+finalbut.addEventListener('click', function () {
+    window.location.reload(true)
+})
 button.addEventListener('click', function () {
     first_div.style.display = "none"
     second_div.style.display = "grid"
@@ -71,19 +75,37 @@ td.forEach(function (el) {
             if (el.classList.contains("for-X") || el.classList.contains("for-o")) {
                 count++
             }
-            console.log(count)
+
+
             let first = new Promise((rev, reg) => {
-                if (count >= 5) {
-                    rev("won")
+                if (count >= 5 && td[0].classList.contains("for-X") && td[1].classList.contains("for-X") && td[2].classList.contains("for-X") || count >= 5 && td[0].classList.contains("for-o") && td[1].classList.contains("for-o") && td[2].classList.contains("for-o")
+                    || count >= 5 && td[0].classList.contains("for-X") && td[4].classList.contains("for-X") && td[8].classList.contains("for-X") || count >= 5 && td[0].classList.contains("for-o") && td[4].classList.contains("for-o") && td[8].classList.contains("for-o")
+                    || count >= 5 && td[0].classList.contains("for-X") && td[3].classList.contains("for-X") && td[6].classList.contains("for-X") || count >= 5 && td[0].classList.contains("for-o") && td[3].classList.contains("for-o") && td[6].classList.contains("for-o")
+                    || count >= 5 && td[3].classList.contains("for-X") && td[4].classList.contains("for-X") && td[5].classList.contains("for-X") || count >= 5 && td[3].classList.contains("for-o") && td[4].classList.contains("for-o") && td[5].classList.contains("for-o")
+                    || count >= 5 && td[6].classList.contains("for-X") && td[7].classList.contains("for-X") && td[8].classList.contains("for-X") || count >= 5 && td[6].classList.contains("for-o") && td[7].classList.contains("for-o") && td[8].classList.contains("for-o")
+                    || count >= 5 && td[1].classList.contains("for-X") && td[4].classList.contains("for-X") && td[7].classList.contains("for-X") || count >= 5 && td[1].classList.contains("for-o") && td[4].classList.contains("for-o") && td[7].classList.contains("for-o")
+                    || count >= 5 && td[2].classList.contains("for-X") && td[5].classList.contains("for-X") && td[8].classList.contains("for-X") || count >= 5 && td[2].classList.contains("for-o") && td[5].classList.contains("for-o") && td[8].classList.contains("for-o")
+                    || count >= 5 && td[2].classList.contains("for-X") && td[4].classList.contains("for-X") && td[6].classList.contains("for-X") || count >= 5 && td[2].classList.contains("for-o") && td[4].classList.contains("for-o") && td[6].classList.contains("for-o")
+
+
+                ) {
+
+                    rev(
+                        endgame.style.display = "flex",
+                        td.forEach(function (el) {
+                            el.classList.remove("can_write")
+                        })
+                    )
+
                 }
                 else {
-                    reg("lose");
+                    reg("rev")
                 }
 
             }
             )
             first.then(
-                (final) => console.log(`you${final}`),
+                (final) => (final),
                 (yy) => console.log(`you${yy}`)
             )
         })
